e65d9bde2bc9c98cd8b5bd0664b0f862d8ad8caa
syd <student@Awen.local>
2016-09-01 17:43:55 +0800

    syd
    xiang

---
+++ B/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  B
+//
+//  Created by student on 16/9/1.
+//  Copyright © 2016年 syd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ B/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  B
+//
+//  Created by student on 16/9/1.
+//  Copyright © 2016年 syd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
